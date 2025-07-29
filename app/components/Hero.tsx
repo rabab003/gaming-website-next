204c0c04f7836f32c7231f942302c1e31ffd4b85
rabab003 <bintaliraq4@gmail.com>
2025-07-29 14:55:09 +0300

    2:55 PM

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import MaxWidthWrapper from "./defaults/MaxWidthWrapper";
+import SwiperCards from "./SwiperCards";
 
 const Hero = () => {
   return (
     <div>
-      <MaxWidthWrapper children={undefined}></MaxWidthWrapper>
+      <MaxWidthWrapper children={undefined}>
+        <SwiperCards
+          items={[{ card: <div className="text-3xl font-bold">Hero</div> }]}
+        >
+          <div className=""></div>
+        </SwiperCards>
+      </MaxWidthWrapper>
     </div>
   );
 };
--- app/components/SwiperCards.tsx
+++ app/components/SwiperCards.tsx
@@ -3,22 +3,35 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import { Pagination } from "swiper/modules";
 
-const SwiperCards = () => {
+const SwiperCards = ({
+  items,
+}: {
+  items: { src: string; card: ReactNode }[];
+  paginationImages?: boolean;
+  className={`w-full ${className || "h-96"}`}
+}) => {
   return (
     <>
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        ...
-      </Swiper>
+      <div className="flex flex-col gap-4">
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          {items.map(({ card }, i) => (
+            <div>{card}</div>
+          ))}
+        </Swiper>
+        {Pagination &&
+          items.map(({ src }, i) => (
+            <div className="w-40 h-40 relative" key={i}>
+              <Image alt="" src={src} fill className="object-cover" />
+            </div>
+          ))}
+      </div>
     </>
   );
 };
